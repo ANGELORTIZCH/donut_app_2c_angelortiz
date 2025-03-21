d92fb307ed3867216e0c957e518618cea4a0b410
ANGELORTIZCH <ortiz1997@outlook.com>
2025-03-21 12:12:08 -0600

    Adding Favorite Button

--- app_donut_angel/lib/utils/donut_tile.dart
+++ app_donut_angel/lib/utils/donut_tile.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-class DonutTile extends StatelessWidget {
+class DonutTile extends StatefulWidget {
   final String donutFlavor;
   final String donutName;
   final String donutPrice;
@@ -20,12 +20,19 @@ class DonutTile extends StatelessWidget {
   });
 
   @override
+  _DonutTileState createState() => _DonutTileState();
+}
+
+class _DonutTileState extends State<DonutTile> {
+  bool isFavorite = false;
+
+  @override
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.all(6.0),
       child: Container(
         decoration: BoxDecoration(
-          color: donutColor[50],
+          color: widget.donutColor[50],
           borderRadius: BorderRadius.circular(24),
         ),
         child: Column(
@@ -37,7 +44,7 @@ class DonutTile extends StatelessWidget {
               children: [
                 Container(
                   decoration: BoxDecoration(
-                    color: donutColor[100],
+                    color: widget.donutColor[100],
                     borderRadius: const BorderRadius.only(
                       bottomLeft: Radius.circular(24),
                       topRight: Radius.circular(24),
@@ -48,11 +55,11 @@ class DonutTile extends StatelessWidget {
                     horizontal: 18,
                   ),
                   child: Text(
-                    '\$$donutPrice',
+                    '\$${widget.donutPrice}',
                     style: TextStyle(
                       fontSize: 18,
                       fontWeight: FontWeight.bold,
-                      color: donutColor[800],
+                      color: widget.donutColor[800],
                     ),
                   ),
                 ),
@@ -65,18 +72,18 @@ class DonutTile extends StatelessWidget {
                   horizontal: 24,
                   vertical: 12,
                 ),
-                child: Image.asset(imageName, fit: BoxFit.contain),
+                child: Image.asset(widget.imageName, fit: BoxFit.contain),
               ),
             ),
             // Donut Flavor Text
             Text(
-              donutFlavor,
+              widget.donutFlavor,
               style: const TextStyle(fontWeight: FontWeight.bold, fontSize: 18),
             ),
             const SizedBox(height: 1),
             // Texto de la tienda
             Text(
-              donutName,
+              widget.donutName,
               style: const TextStyle(fontSize: 12),
             ),
             // Ícono de corazón + Botón Add
@@ -85,9 +92,22 @@ class DonutTile extends StatelessWidget {
               child: Row(
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
-                  const Icon(Icons.favorite_border),
+                  IconButton(
+                    icon: Icon(
+                      isFavorite ? Icons.favorite : Icons.favorite_border,
+                      color: isFavorite
+                          ? Colors.red
+                          : null, // Rojo si está relleno
+                    ),
+                    onPressed: () {
+                      setState(() {
+                        isFavorite =
+                            !isFavorite; // Alterna entre favorito y no favorito
+                      });
+                    },
+                  ),
                   GestureDetector(
-                    onTap: onAddToCart,
+                    onTap: widget.onAddToCart,
                     child: const Text(
                       "Add",
                       style: TextStyle(
